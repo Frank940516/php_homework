d3a690aa7d4fc027e34b9fe38a99dc82d4677c1e
Frank940516 <a1123302@mail.nuk.edu.tw>
2025-03-12 21:13:35 +0800

    Update php_homework submodule

---
+++ info.php
@@ -0,0 +1,16 @@
+<?php
+$uName = $_POST['uName'];
+$uID = $_POST['uID'];
+$uPhone = $_POST['uPhone'];
+$uEmail = $_POST['uEmail'];
+$uGender = $_POST['uGender'];
+$uJoin = $_POST['uJoin'];
+echo "姓名:" .$uName."<br>";
+echo "學號:" .$uID."<br>";
+echo "連絡電話:" .$uPhone."<br>";
+echo "信箱:" .$uEmail."<br>";
+echo "性別:" .$uGender."<br>";
+echo "是否參加:" .$uJoin."<br>";
+
+
+?>
